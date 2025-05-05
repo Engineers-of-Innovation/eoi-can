f9862f004954804fe4dbfc2c164d9b5d76c224fa
Floris Romeijn <fromeijn@rocsys.com>
2025-05-05 17:45:45 +0200

    feat: add script to build and send binaries to datalogger

---
+++ build-and-send-to-datalogger.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This script builds the eoi-gnss-to-can and eoi-can-display-framebuffer projects for the datalogger
+# and sends the binaries to the datalogger.
+# Make sure you have cross and cargo installed (cargo install cross)
+#
+# argument is the ip address of the datalogger
+# usage: ./build-and-send-to-datalogger.sh <ip_address>
+
+# check if valid ip address is provided
+if [[ ! $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "Usage: $0 <ip_address>"
+  exit 1
+fi
+
+# building
+cd eoi-gnss-to-can
+cross build --target armv7-unknown-linux-gnueabihf --release
+cd ..
+
+cd eoi-can-display-framebuffer
+cross build --target armv7-unknown-linux-gnueabihf --release
+cd ..
+
+# sending, make sure you have you ssh keys set up in the datalogger, you might need to run ssh-copy-id
+
+scp target/armv7-unknown-linux-gnueabihf/release/eoi-can-display-framebuffer root@${1}:~
+scp target/armv7-unknown-linux-gnueabihf/release/eoi-gnss-to-can root@${1}:~
