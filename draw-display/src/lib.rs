e258538fb2448378b803c7293a41396fe581b6aa
Floris Romeijn <fromeijn@rocsys.com>
2025-05-17 12:40:03 +0200

    chore: update mppt channels

--- draw-display/src/lib.rs
+++ draw-display/src/lib.rs
@@ -181,13 +181,13 @@ impl DisplayData {
                     // we map the mppt channel to the index of the (solar) array
                     // mppt id is what it is, but channel and panel id are indexed from 0
                     if let Some(panel_id) = match (mppt_data.mppt_id, mppt_channel) {
-                        (7, 0) => Some(0),
-                        (7, 1) => Some(1),
-                        (7, 2) => Some(2),
+                        (3, 0) => Some(0),
+                        (3, 1) => Some(1),
+                        (3, 3) => Some(2),
                         (5, 0) => Some(3),
                         (5, 1) => Some(4),
                         (5, 2) => Some(5),
-                        (4, 0) => Some(6),
+                        (4, 1) => Some(6),
                         (4, 2) => Some(7),
                         (2, 0) => Some(8),
                         (2, 1) => Some(9),
