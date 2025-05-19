e6227d2d08954f6d5300f1f7448bca8144c346c5
Floris Romeijn <fromeijn@rocsys.com>
2025-05-19 17:49:49 +0200

    feat: make build-and-send.sh more universal

--- build-and-send.sh
+++ build-and-send.sh
@@ -6,10 +6,10 @@
 # argument is the ip address of the datalogger
 # usage: ./build-and-send-to-datalogger.sh <ip_address>
 
-# check if valid ip address is provided
-if [[ ! $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-  echo "Usage: $0 <ip_address>"
-  exit 1
+# check if user@address is provided, e.g root@192.168.0.1 or foo@bar.com
+if [[ ! "$1" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+$ ]]; then
+    echo "Usage: $0 <user@ip_address>"
+    exit 1
 fi
 
 # building
@@ -23,5 +23,5 @@ cd ..
 
 # sending, make sure you have you ssh keys set up in the datalogger, you might need to run ssh-copy-id
 
-scp target/armv7-unknown-linux-gnueabihf/release/eoi-can-display-framebuffer root@${1}:~
-scp target/armv7-unknown-linux-gnueabihf/release/eoi-gnss-to-can root@${1}:~
+scp target/armv7-unknown-linux-gnueabihf/release/eoi-can-display-framebuffer ${1}:~
+scp target/armv7-unknown-linux-gnueabihf/release/eoi-gnss-to-can ${1}:~
